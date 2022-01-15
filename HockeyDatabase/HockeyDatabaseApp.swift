dcb17b3ec38cef8e004ca6a337b574709c28d8f0
Larry Burris <lburris@mac.com>
2022-01-15 17:04:36 -0500

    Initial Commit

---
+++ HockeyDatabase/HockeyDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyDatabaseApp.swift
+//  HockeyDatabase
+//
+//  Created by Larry Burris on 1/15/22.
+//
+
+import SwiftUI
+
+@main
+struct HockeyDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
